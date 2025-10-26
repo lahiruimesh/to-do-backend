9c0a401670aea9df25dbe91e786011ce4004f6ed
lahiruimesh <imeshbl.22@uom.lk>
2025-10-26 15:13:03 +0530

    made some changes

--- src/db.js
+++ src/db.js
@@ -3,6 +3,5 @@ const config = require('../knexfile');
 
 const environment = process.env.NODE_ENV || 'development';
 const db = knex(config[environment]);
-// Updated for SSL fix
 
 module.exports = db;
